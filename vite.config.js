506050886f4d91d7e99ea3514d7c9a20d8d5ce59
JhonZ <jonjjj19@gmail.com>
2026-07-23 16:32:27 -0500

    se corrigue el bug de paginas no encontardas error 404, por motivos de produccion

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  // Absolute base so assets resolve correctly on /contacto, /sobre-nosotros, etc.
+  base: '/',
 })
 
 
